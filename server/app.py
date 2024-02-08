38bf1b7b3ce7d7b1ba8bfbe98e77edfe4bb95f89
Connor <connordbrown@gmail.com>
2024-02-08 15:41:10 -0700

    create table users

--- server/app.py
+++ server/app.py
@@ -1,4 +1,11 @@
-from config import app
+from flask import request, session, make_response
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+from datetime import datetime
+
+from config import app, db, api
+from models.User import User
+
 
 if __name__ == "__main__":
   app.run(port=5555, debug=True)
--- server/models/User.py
+++ server/models/User.py
@@ -4,3 +4,13 @@ from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db, bcrypt
+
+##### User Model #####
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
