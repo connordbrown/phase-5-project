0b1d5ae5e98b5464080fe5efad89bc693dcf3b48
Connor <connordbrown@gmail.com>
2024-02-08 16:40:58 -0700

    install Faker, initial seed.py

--- server/seed.py
+++ server/seed.py
@@ -1,4 +1,12 @@
+from faker import Faker
+from datetime import datetime
+
 from config import app, db
+
+from models.User import User
+from models.Category import Category
+from models.Article import Article
+from models.Tag import Tag
 
 if __name__ == "__main__":
   with app.app_context():
