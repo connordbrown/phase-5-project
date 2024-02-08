15f14a44993bd4c3190c296b5957781a761df040
Connor <connordbrown@gmail.com>
2024-02-08 15:25:16 -0700

    set up models imports

---
+++ server/models/ArticleTag.py
@@ -0,0 +1,3 @@
+from sqlalchemy.ext.associationproxy import association_proxy
+
+from config import db
---
+++ server/models/User.py
@@ -0,0 +1,6 @@
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+
+from config import db, bcrypt
