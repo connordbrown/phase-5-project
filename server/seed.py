9667e44507504b301d262c351ee8b94f51a58738
Connor <connordbrown@gmail.com>
2024-02-08 17:46:54 -0700

    add seed data for User model

--- server/seed.py
+++ server/seed.py
@@ -10,5 +10,42 @@ from models.Tag import Tag
 
 if __name__ == "__main__":
   with app.app_context():
-    pass
-    # remove pass and write your seed data
+    print("Deleting all records...")
+    User.query.delete()
+
+    # create Faker instance
+    fake = Faker()
+
+
+    ##### User seed data #####
+    print("Creating users...")
+    users = []
+    usernames = []
+    emails = []
+
+    for i in range(10):
+      # ensure usernames are unique
+      username = fake.first_name()
+      while username in usernames:
+        username = fake.first_name()
+      usernames.append(username)
+
+      email = fake.email()
+      while email in emails:
+        email = fake.email()
+      emails.append(email)
+
+      user = User(
+        username=username,
+        age=fake.random_int(min=10, max=120),
+        email=email,
+      )
+      user.password_hash = user.username + 'password'
+
+      users.append(user)
+
+    db.session.add_all(users)
+
+
+    db.session.commit()  
+    print("Seeding complete.")
