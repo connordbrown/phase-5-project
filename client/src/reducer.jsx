26ec9a784865fae291ce6bbd40219cb10a5b06c9
Connor <connordbrown@gmail.com>
2024-02-14 17:53:54 -0700

    create currentUser slice, add to reducer.jsx

--- client/src/reducer.jsx
+++ client/src/reducer.jsx
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import usersReducer from './slices/usersSlice';
 import loggedInReducer from './slices/loggedInSlice';
+import currentUserReducer from './slices/currentUserSlice';
 
 const rootReducer = combineReducers({
     users: usersReducer,
     loggedIn: loggedInReducer,
+    currentUser: currentUserReducer,
 })
 
 export default rootReducer;
