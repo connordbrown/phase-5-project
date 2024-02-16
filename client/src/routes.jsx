f377687e4eff4d39142ea49eadd8f2f08ab81337
Connor <connordbrown@gmail.com>
2024-02-15 17:33:58 -0700

    add NavBar files, adjust App.jsx

---
+++ client/src/routes.jsx
@@ -0,0 +1,34 @@
+import App from './App';
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
+import UserProfile from './pages/UserProfile';
+import ErrorPage from './pages/ErrorPage';
+
+const routes = [
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/about",
+                element: <About />
+            },
+            {
+                path: "/login",
+                element: <Login />
+            },
+            {
+                path: "profile/:id",
+                element: <UserProfile />
+            }
+        ]
+    }
+]
+
+export default routes;
