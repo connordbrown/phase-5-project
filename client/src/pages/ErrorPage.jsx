0da7f9c88fd0225b7168eecfd932fb7ee2c75dc5
Connor <connordbrown@gmail.com>
2024-02-15 17:24:03 -0700

    set up initial page files

---
+++ client/src/pages/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import { useRouteError } from "react-router-dom";
+
+function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div>
+      <h1>Whoops! Something went wrong!</h1>
+    </div>
+  )
+}
+
+export default ErrorPage;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+function Login() {
+    return (
+      <div>
+        <h1>This is my login component!</h1>
+      </div>
+    )
+}
+  
+export default Login;
