a363b4f46fd3b1cc9411eea6dade747ecc17a1eb
Connor <connordbrown@gmail.com>
2024-02-15 17:16:55 -0700

    set up main.jsx for routing, add files to pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducer';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import routes from './routes';
 import App from './App';
 import './index.css';
 
@@ -11,10 +13,14 @@ const store = configureStore({
   reducer: rootReducer,
 })
 
+const router = createBrowserRouter(routes);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <RouterProvider router={router}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </RouterProvider>
   </Provider>
 )
