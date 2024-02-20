8b60c40c025a42f2b44ebe65d51eb23e956f2e33
Connor <connordbrown@gmail.com>
2024-02-19 20:09:18 -0700

    add details to ArticleInfo page

--- client/src/pages/ArticleInfo.jsx
+++ client/src/pages/ArticleInfo.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { useParams } from 'react-router-dom';
+import { useSelector } from "react-redux";
 
 function ArticleInfo() {
 
-    return (
-        <div>
+  // get specific endpoint - useParams() returns string
+  const params = useParams();
 
-        </div>
-    )
+  // access Redux store
+  const articles = useSelector((state) => state.articles.value);
+
+  const displayArticle = articles.find(article => {
+    return article.id === parseInt(params.id);
+  });
+
+  return (
+    <div>
+    <h2>{displayArticle.title}</h2>
+    <span>by {displayArticle.user.username} - {displayArticle.timestamp}</span>
+    <p>{displayArticle.content}</p>
+    </div>
+  )
 }
 
 export default ArticleInfo;
