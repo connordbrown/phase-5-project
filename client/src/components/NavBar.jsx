7ef577d3d1d2aa8f3a013fd1ddddb7d45b6eb41d
Connor <connordbrown@gmail.com>
2024-02-24 16:39:50 -0700

    update UserProfile component

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 // enable links to other pages and .active styling
 import { NavLink } from 'react-router-dom';
+// access Redux
+import { useSelector } from 'react-redux';
 // styling for NavBar
 import './styling/NavBar.css';
 
 // create nav bar multiple endpoints
 function NavBar() {
+
+    // access Redux store
+    const currentUser = useSelector((state) => state.currentUser.value);
+
     return (
         <div className='nav-container'>
             <nav className='navbar'>
                 <NavLink to='/'>Home</NavLink>
                 <NavLink to='/about'>About</NavLink>
                 <NavLink to='/write'>Write</NavLink>
+                <NavLink to={`/profile/${currentUser.id}`}>Profile</NavLink>
                 <NavLink to='/logout'>Logout</NavLink>
             </nav>
         </div>
--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import './styling/UserProfile.css';
 
 function UserProfile() {
+
+  const currentUser = useSelector((state) => state.currentUser.value);
+  const articles = useSelector((state) => state.articles.value);
+  const articlesLoaded = useSelector((state) => state.articlesLoaded.value);
+
+  const userArticles = articles.filter(article => {
+    return article.user_id === currentUser.id;
+  })
+
+  if (!articlesLoaded) {
+    return <h2>Loading...</h2>;
+  }
+
   return(
-    <div>
+    <div className='user-info'>
+      <div>
       <h1>User Profile</h1>
+      <h2>{currentUser.username}</h2>
+      <p>Email: {currentUser.email}</p>
+      <p>Age: {currentUser.age}</p>
+      <h2>My articles</h2>
+    </div>
+      <ul>
+        {userArticles.map(article => {
+          return (
+            <div key={article.id}>
+              <Link to={`/articles/${article.id}`} className='article-link'>
+                <li key={article.id} className='article'>{article.title}</li>
+              </Link>
+            </div>
+          )
+        })}
+      </ul>
     </div>
   );
 };
