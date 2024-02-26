b1d8c14a3e166779a41f5f7707e10ecdc8399dab
Connor <connordbrown@gmail.com>
2024-02-26 15:11:13 -0700

    add search feature/styling to ArticleList

--- client/src/components/ArticleList.jsx
+++ client/src/components/ArticleList.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './styling/ArticleList.css';
 
 function ArticleList() {
 
+  const [searchItem, setSearchItem] = useState("");
   const articles = useSelector((state) => state.articles.value)
+
+  const articlesToView = articles.filter(article => {
+    return article.tags.some(tag => tag.title.includes(searchItem));
+  })
 
   return (
     <div>
+      <div id='search-form'>
+        <label htmlFor='search'>Filter by tags:</label>
+        <form>
+          <input
+            id='search'
+            name='search'
+            type='text'
+            placeholder='Search by tags'
+            onChange={(e) => setSearchItem(e.target.value)}
+            value={searchItem}></input>
+        </form>
+      </div>
       <ul>
-        {articles.map(article => (
+        {articlesToView.map(article => (
           <Link to={`/articles/${article.id}`} key={article.id} className='article-link'>
             <li className='article' key={article.id}>
               <span>
