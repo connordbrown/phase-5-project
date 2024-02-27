6394d1504c4aa949af4479cadeba0b78c12a388c
Connor <connordbrown@gmail.com>
2024-02-27 15:08:45 -0700

    add last-minute fixes

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './styling/About.css'
 
 // display information about application
 function About() {
     return (
-      <div>
+      <div className='about'>
         <h2>About This App</h2>
         <p>This is ArticleWriter, the handy app for creating your very own articles!
           All you have to do is sign up, log in, and start writing!
